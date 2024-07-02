4ce9a03b20980c8f8d398331d97b09ef317d2dbd
mcdmaisd <tkim0156@gmail.com>
2024-07-02 18:52:20 +0900

    Initial Commit

---
+++ ReminderClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReminderClone
+//
+//  Created by ilim on 2024-07-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
